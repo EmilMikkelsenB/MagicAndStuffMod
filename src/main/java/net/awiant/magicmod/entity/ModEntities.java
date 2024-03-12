f7c6dc0c7c642e2635334dfd9a90c5bd1b36baa7
awiant <emil.mikkelsenbage15@gmail.com>
2024-03-12 21:58:24 +0100

    Revert "edit to modentites and bolt"
    This reverts commit 4dcb38a5518c3e93cd230195b1840a7dd7c591b9.

--- src/main/java/net/awiant/magicmod/entity/ModEntities.java
+++ src/main/java/net/awiant/magicmod/entity/ModEntities.java
@@ -15,11 +15,7 @@ public class ModEntities {
     public static DeferredRegister<EntityType<?>> ENTITY_TYPES =
             DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, MagicMod.MOD_ID);
 
-    public static final RegistryObject<EntityType<MagicBolt>> MAGIC_BOLT = ENTITY_TYPES.register("magic_bolt",
-            () -> EntityType.Builder.<MagicBolt>of((entity, world) -> new MagicBolt(entity, world, BlockPos.ZERO, Vec3.ZERO), MobCategory.MISC)
-                    .sized(0.6F, 1.8F) // Set the size of the entity
-                    .clientTrackingRange(10) // Set the tracking range for the client
-                    .build("magicmod:magic_bolt")); // Build the entity type with the specified id
+
 
     public static void register(IEventBus eventBus) {
         ENTITY_TYPES.register(eventBus);
--- src/main/java/net/awiant/magicmod/entity/custom/MagicBolt.java
+++ src/main/java/net/awiant/magicmod/entity/custom/MagicBolt.java
@@ -35,6 +35,7 @@ public class MagicBolt extends Entity {
 
     @Override
     protected void defineSynchedData() {
+
     }
 
     @Override
